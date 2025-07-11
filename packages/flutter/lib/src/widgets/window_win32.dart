76f549515a12f1f69d93a8d5a79f12274f969af3
Matt Kosarek <matt.kosarek@canonical.com>
2025-07-11 14:38:14 -0400

    framework: update win32 API for dialogs

--- packages/flutter/lib/src/widgets/window_win32.dart
+++ packages/flutter/lib/src/widgets/window_win32.dart
@@ -12,6 +12,8 @@ import 'binding.dart';
 import 'window.dart';
 import 'window_positioner.dart';
 
+typedef HWND = Pointer<Void>;
+
 /// Handler for Win32 messages.
 abstract class WindowsMessageHandler {
   /// Handles a window message. Returned value, if not null will be
@@ -54,9 +56,11 @@ class WindowingOwnerWin32 extends WindowingOwner {
     required DialogWindowControllerDelegate delegate,
     FlutterView? parent,
   }) {
-    throw UnsupportedError(
-      'Current platform does not support windowing.\n'
-      'Implement a WindowingDelegate for this platform.',
+    return DialogWindowControllerWin32(
+      owner: this,
+      delegate: delegate,
+      contentSize: contentSize,
+      parent: parent,
     );
   }
 
@@ -122,6 +126,141 @@ class WindowingOwnerWin32 extends WindowingOwner {
   external static void _initializeWindowing(int engineId, Pointer<_WindowingInitRequest> request);
 }
 
+class _HwndWrapper {
+  _HwndWrapper({required this.hwnd});
+
+  @Native<Pointer<Void> Function(Int64, Int64)>(symbol: 'InternalFlutterWindows_WindowManager_GetTopLevelWindowHandle')
+  external static Pointer<Void> getWindowHandle(int engineId, int viewId);
+
+  @Native<Void Function(Pointer<Void>)>(symbol: 'DestroyWindow')
+  external static void _destroyWindow(Pointer<Void> windowHandle);
+
+  @Native<_Size Function(Pointer<Void>)>(symbol: 'InternalFlutterWindows_WindowManager_GetWindowContentSize')
+  external static _Size _getWindowContentSize(Pointer<Void> windowHandle);
+
+  @Native<Void Function(Pointer<Void>, Pointer<ffi.Utf16>)>(symbol: 'SetWindowTextW')
+  external static void _setWindowTitle(Pointer<Void> windowHandle, Pointer<ffi.Utf16> title);
+
+  @Native<Void Function(Pointer<Void>, Pointer<_Sizing>)>(symbol: 'InternalFlutterWindows_WindowManager_SetWindowContentSize')
+  external static void _setWindowContentSize(Pointer<Void> windowHandle, Pointer<_Sizing> size);
+
+  @Native<Void Function(Pointer<Void>, Int32)>(symbol: 'ShowWindow')
+  external static void _showWindow(Pointer<Void> windowHandle, int command);
+
+  @Native<Int32 Function(Pointer<Void>)>(symbol: 'IsIconic')
+  external static int _isIconic(Pointer<Void> windowHandle);
+
+  @Native<Int32 Function(Pointer<Void>)>(symbol: 'IsZoomed')
+  external static int _isZoomed(Pointer<Void> windowHandle);
+
+  @Native<Pointer<Void> Function(Pointer<Void>, Uint32)>(symbol: 'GetWindow')
+  external static Pointer<Void> _getWindow(Pointer<Void> windowHandle, int cmd);
+
+  Size contentSize() {
+    _ensureNotDestroyed();
+    final _Size size = _getWindowContentSize(hwnd);
+    final Size result = Size(size.width, size.height);
+    return result;
+  }
+
+  void setTitle(String title) {
+    _ensureNotDestroyed();
+    final Pointer<ffi.Utf16> titlePointer = title.toNativeUtf16();
+    _setWindowTitle(hwnd, titlePointer);
+    ffi.calloc.free(titlePointer);
+  }
+
+  void updateContentSize(WindowSizing sizing) {
+    _ensureNotDestroyed();
+    final Pointer<_Sizing> ffiSizing = ffi.calloc<_Sizing>();
+    ffiSizing.ref.set(sizing);
+    _setWindowContentSize(hwnd, ffiSizing);
+    ffi.calloc.free(ffiSizing);
+  }
+
+  void activate() {
+    _ensureNotDestroyed();
+    _showWindow(hwnd, SW_RESTORE);
+  }
+
+  bool isFullscreen() {
+    return false;
+  }
+
+  void setFullscreen(bool fullscreen, {int? displayId}) {}
+
+  bool isMaximized() {
+    _ensureNotDestroyed();
+    return _isZoomed(hwnd) != 0;
+  }
+
+  bool isMinimized() {
+    _ensureNotDestroyed();
+    return _isIconic(hwnd) != 0;
+  }
+
+  void setMinimized(bool minimized) {
+    _ensureNotDestroyed();
+    if (minimized) {
+      _showWindow(hwnd, SW_MINIMIZE);
+    } else {
+      _showWindow(hwnd, SW_RESTORE);
+    }
+  }
+
+  void setMaximized(bool maximized) {
+    _ensureNotDestroyed();
+    if (maximized) {
+      _showWindow(hwnd, SW_MAXIMIZE);
+    } else {
+      _showWindow(hwnd, SW_RESTORE);
+    }
+  }
+
+  void _ensureNotDestroyed() {
+    if (_destroyed) {
+      throw StateError('Window has been destroyed.');
+    }
+  }
+
+  HWND? get parent {
+    _ensureNotDestroyed();
+    return _getWindow(hwnd, _GW_OWNER);
+  }
+
+  bool destroy() {
+    if (_destroyed) {
+      return false;
+    }
+    _destroyWindow(hwnd);
+    _destroyed = true;
+    return true;
+  }
+
+  /// Marks the HWND as destroyed. This is used when destruction is driven by the
+  /// native side, for example when the owner is destroyed.
+  /// Returns true if the window was not destroyed before, false otherwise.
+  bool markDestroyed() {
+    if (_destroyed) {
+      return false;
+    }
+    _destroyed = true;
+    return true;
+  }
+
+  final HWND hwnd;
+  bool _destroyed = false;
+  static const int _WM_SIZE = 0x0005;
+  static const int _WM_CLOSE = 0x0010;
+  static const int _WM_DESTROY = 0x0002;
+
+  static const int SW_RESTORE = 9;
+  static const int SW_MAXIMIZE = 3;
+  static const int SW_MINIMIZE = 6;
+
+  static const int _GW_OWNER = 4;
+}
+
 /// The Win32 implementation of the regular window controller.
 class RegularWindowControllerWin32 extends RegularWindowController
     implements WindowsMessageHandler {
@@ -135,119 +274,84 @@ class RegularWindowControllerWin32 extends RegularWindowController
        _delegate = delegate,
        super.empty() {
     owner.addMessageHandler(this);
-    final Pointer<_WindowCreationRequest> request =
-        ffi.calloc<_WindowCreationRequest>()..ref.contentSize.set(contentSize);
+    final Pointer<_RegularWindowCreationRequest> request =
+        ffi.calloc<_RegularWindowCreationRequest>()..ref.contentSize.set(contentSize);
     final int viewId = _createWindow(PlatformDispatcher.instance.engineId!, request);
     ffi.calloc.free(request);
     final FlutterView flutterView = WidgetsBinding.instance.platformDispatcher.views.firstWhere(
       (FlutterView view) => view.viewId == viewId,
     );
     setView(flutterView);
+    _hwnd = _HwndWrapper(
+      hwnd: _HwndWrapper.getWindowHandle(PlatformDispatcher.instance.engineId!, flutterView.viewId),
+    );
   }
 
   @override
   Size get contentSize {
-    _ensureNotDestroyed();
-    final _Size size = _getWindowContentSize(getWindowHandle());
-    final Size result = Size(size.width, size.height);
-    return result;
+    return _hwnd.contentSize();
   }
 
   @override
   void setTitle(String title) {
-    _ensureNotDestroyed();
-    final Pointer<ffi.Utf16> titlePointer = title.toNativeUtf16();
-    _setWindowTitle(getWindowHandle(), titlePointer);
-    ffi.calloc.free(titlePointer);
+    _hwnd.setTitle(title);
   }
 
   @override
   void updateContentSize(WindowSizing sizing) {
-    _ensureNotDestroyed();
-    final Pointer<_Sizing> ffiSizing = ffi.calloc<_Sizing>();
-    ffiSizing.ref.set(sizing);
-    _setWindowContentSize(getWindowHandle(), ffiSizing);
-    ffi.calloc.free(ffiSizing);
+    _hwnd.updateContentSize(sizing);
   }
 
   @override
   void activate() {
-    _ensureNotDestroyed();
-    _showWindow(getWindowHandle(), SW_RESTORE);
+    _hwnd.activate();
   }
 
   @override
   bool isFullscreen() {
-    return false;
+    return _hwnd.isFullscreen();
   }
 
   @override
-  void setFullscreen(bool fullscreen, {int? displayId}) {}
+  void setFullscreen(bool fullscreen, {int? displayId}) {
+    _hwnd.setFullscreen(fullscreen, displayId: displayId);
+  }
 
   @override
   bool isMaximized() {
-    _ensureNotDestroyed();
-    return _isZoomed(getWindowHandle()) != 0;
+    return _hwnd.isMaximized();
   }
 
   @override
   bool isMinimized() {
-    _ensureNotDestroyed();
-    return _isIconic(getWindowHandle()) != 0;
+    return _hwnd.isMinimized();
   }
 
   @override
   void setMinimized(bool minimized) {
-    _ensureNotDestroyed();
-    if (minimized) {
-      _showWindow(getWindowHandle(), SW_MINIMIZE);
-    } else {
-      _showWindow(getWindowHandle(), SW_RESTORE);
-    }
+    _hwnd.setMinimized(minimized);
   }
 
   @override
   void setMaximized(bool maximized) {
-    _ensureNotDestroyed();
-    if (maximized) {
-      _showWindow(getWindowHandle(), SW_MAXIMIZE);
-    } else {
-      _showWindow(getWindowHandle(), SW_RESTORE);
-    }
+    _hwnd.setMaximized(maximized);
   }
 
   /// Returns HWND pointer to the top level window.
-  Pointer<Void> getWindowHandle() {
-    _ensureNotDestroyed();
-    return _getWindowHandle(PlatformDispatcher.instance.engineId!, rootView.viewId);
+  HWND get hwnd {
+    return _hwnd.hwnd;
   }
 
-  void _ensureNotDestroyed() {
-    if (_destroyed) {
-      throw StateError('Window has been destroyed.');
-    }
-  }
-
-  final RegularWindowControllerDelegate _delegate;
-  bool _destroyed = false;
+  @override
+  bool get destroyed => _hwnd._destroyed;
 
   @override
   void destroy() {
-    if (_destroyed) {
-      return;
+    if (_hwnd.destroy()) {
+      _delegate.onWindowDestroyed();
+      _owner.removeMessageHandler(this);
     }
-    _destroyWindow(getWindowHandle());
-    _destroyed = true;
-    _delegate.onWindowDestroyed();
-    _owner.removeMessageHandler(this);
   }
-
-  static const int _WM_SIZE = 0x0005;
-  static const int _WM_CLOSE = 0x0010;
-
-  static const int SW_RESTORE = 9;
-  static const int SW_MAXIMIZE = 3;
-  static const int SW_MINIMIZE = 6;
 
   @override
   int? handleWindowsMessage(
@@ -261,54 +365,151 @@ class RegularWindowControllerWin32 extends RegularWindowController
       return null;
     }
 
-    if (message == _WM_CLOSE) {
+    if (message == _HwndWrapper._WM_DESTROY && _hwnd.markDestroyed()) {
+      _delegate.onWindowDestroyed();
+    } else if (message == _HwndWrapper._WM_CLOSE) {
       _delegate.onWindowCloseRequested(this);
       return 0;
-    } else if (message == _WM_SIZE) {
+    } else if (message == _HwndWrapper._WM_SIZE) {
       notifyListeners();
     }
     return null;
   }
 
+  final RegularWindowControllerDelegate _delegate;
   final WindowingOwnerWin32 _owner;
+  late final _HwndWrapper _hwnd;
 
-  @Native<Int64 Function(Int64, Pointer<_WindowCreationRequest>)>(
+  @Native<Int64 Function(Int64, Pointer<_RegularWindowCreationRequest>)>(
     symbol: 'InternalFlutterWindows_WindowManager_CreateRegularWindow',
   )
-  external static int _createWindow(int engineId, Pointer<_WindowCreationRequest> request);
+  external static int _createWindow(int engineId, Pointer<_RegularWindowCreationRequest> request);
+}
 
-  @Native<Pointer<Void> Function(Int64, Int64)>(
-    symbol: 'InternalFlutterWindows_WindowManager_GetTopLevelWindowHandle',
-  )
-  external static Pointer<Void> _getWindowHandle(int engineId, int viewId);
+/// The Win32 implementation of the dialog window controller.
+class DialogWindowControllerWin32 extends DialogWindowController implements WindowsMessageHandler {
+  /// Creates a new dialog window controller for Win32. When this constructor
+  /// completes the FlutterView is created and framework is aware of it.
+  DialogWindowControllerWin32({
+    required WindowingOwnerWin32 owner,
+    required DialogWindowControllerDelegate delegate,
+    required WindowSizing contentSize,
+    FlutterView? parent,
+  }) : _owner = owner,
+       _delegate = delegate,
+       super.empty() {
+    final int engineId = PlatformDispatcher.instance.engineId!;
 
-  @Native<Void Function(Pointer<Void>)>(symbol: 'DestroyWindow')
-  external static void _destroyWindow(Pointer<Void> windowHandle);
+    // If the parent is minimized, restore it to prevent the modal dialog from being hidden.
+    _HwndWrapper? parentWindow;
+    if (parent != null) {
+      parentWindow = _HwndWrapper(hwnd: _HwndWrapper.getWindowHandle(engineId, parent.viewId));
+      if (parentWindow.isMinimized()) {
+        setMinimized(true);
+      }
+    }
 
-  @Native<_Size Function(Pointer<Void>)>(
-    symbol: 'InternalFlutterWindows_WindowManager_GetWindowContentSize',
-  )
-  external static _Size _getWindowContentSize(Pointer<Void> windowHandle);
-
-  @Native<Void Function(Pointer<Void>, Pointer<ffi.Utf16>)>(symbol: 'SetWindowTextW')
-  external static void _setWindowTitle(Pointer<Void> windowHandle, Pointer<ffi.Utf16> title);
-
-  @Native<Void Function(Pointer<Void>, Pointer<_Sizing>)>(
-    symbol: 'InternalFlutterWindows_WindowManager_SetWindowContentSize',
-  )
-  external static void _setWindowContentSize(Pointer<Void> windowHandle, Pointer<_Sizing> size);
-
-  @Native<Void Function(Pointer<Void>, Int32)>(symbol: 'ShowWindow')
-  external static void _showWindow(Pointer<Void> windowHandle, int command);
-
-  @Native<Int32 Function(Pointer<Void>)>(symbol: 'IsIconic')
-  external static int _isIconic(Pointer<Void> windowHandle);
-
-  @Native<Int32 Function(Pointer<Void>)>(symbol: 'IsZoomed')
-  external static int _isZoomed(Pointer<Void> windowHandle);
+    final Pointer<_DialogWindowCreationRequest> request =
+        ffi.calloc<_DialogWindowCreationRequest>()
+          ..ref.contentSize.set(contentSize)
+          ..ref.parentWindow = parentWindow?.hwnd ?? nullptr;
+    final int viewId = _createWindow(engineId, request);
+    ffi.calloc.free(request);
+    final FlutterView flutterView = WidgetsBinding.instance.platformDispatcher.views.firstWhere(
+      (FlutterView view) => view.viewId == viewId,
+    );
+    setView(flutterView);
+    owner.addMessageHandler(this);
+    _hwnd = _HwndWrapper(hwnd: _HwndWrapper.getWindowHandle(engineId, viewId));
+  }
 
   @override
-  bool get destroyed => _destroyed;
+  Size get contentSize {
+    return _hwnd.contentSize();
+  }
+
+  @override
+  FlutterView? get parent {
+    final HWND? owner = _hwnd.parent;
+    if (owner == nullptr) {
+      return null;
+    }
+
+    final int engineId = PlatformDispatcher.instance.engineId!;
+    return PlatformDispatcher.instance.views.cast<FlutterView?>().firstWhere(
+      (FlutterView? view) => _HwndWrapper.getWindowHandle(engineId, view!.viewId) == owner,
+      orElse: () => null,
+    );
+  }
+
+  @override
+  void setTitle(String title) {
+    _hwnd.setTitle(title);
+  }
+
+  @override
+  void updateContentSize(WindowSizing size) {
+    _hwnd.updateContentSize(size);
+  }
+
+  @override
+  void setMinimized(bool minimized) {
+    _hwnd.setMinimized(minimized);
+  }
+
+  @override
+  bool isMinimized() {
+    return _hwnd.isMinimized();
+  }
+
+  /// Returns HWND pointer to the top level window.
+  Pointer<Void> getWindowHandle() {
+    return _hwnd.hwnd;
+  }
+
+  @override
+  bool get destroyed => _hwnd._destroyed;
+
+  @override
+  void destroy() {
+    if (_hwnd.destroy()) {
+      _delegate.onWindowDestroyed();
+      _owner.removeMessageHandler(this);
+    }
+  }
+
+  @override
+  int? handleWindowsMessage(
+    FlutterView view,
+    Pointer<Void> windowHandle,
+    int message,
+    int wParam,
+    int lParam,
+  ) {
+    if (view.viewId != rootView.viewId) {
+      return null;
+    }
+
+    // Window will be destroyed from native side when owner is destroyed.
+    if (message == _HwndWrapper._WM_DESTROY && _hwnd.markDestroyed()) {
+      _delegate.onWindowDestroyed();
+    } else if (message == _HwndWrapper._WM_CLOSE) {
+      _delegate.onWindowCloseRequested(this);
+      return 0;
+    } else if (message == _HwndWrapper._WM_SIZE) {
+      notifyListeners();
+    }
+    return null;
+  }
+
+  final DialogWindowControllerDelegate _delegate;
+  final WindowingOwnerWin32 _owner;
+  late final _HwndWrapper _hwnd;
+
+  @Native<Int64 Function(Int64, Pointer<_DialogWindowCreationRequest>)>(
+    symbol: 'InternalFlutterWindows_WindowManager_CreateDialogWindow',
+  )
+  external static int _createWindow(int engineId, Pointer<_DialogWindowCreationRequest> request);
 }
 
 /// Request to initialize windowing system.
@@ -364,8 +565,13 @@ final class _Sizing extends Struct {
   }
 }
 
-final class _WindowCreationRequest extends Struct {
+final class _RegularWindowCreationRequest extends Struct {
   external _Sizing contentSize;
+}
+
+final class _DialogWindowCreationRequest extends Struct {
+  external _Sizing contentSize;
+  external Pointer<Void> parentWindow;
 }
 
 /// Windows message received for all top level windows (regardless whether
